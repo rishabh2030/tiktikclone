606011999666c281d1106d3262a0bf4a66a5079e
quoc thai <lqthai5121@gmail.com>
2023-02-01 21:04:27 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["img-cdn.2game.vn", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

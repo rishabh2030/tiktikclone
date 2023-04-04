5c024efbf7aa0c19d4125f1cb2bf1e5abc0fbfcd
rishabh2030 <rishabhsharma1@ssipmt.com>
2023-04-04 21:24:37 +0530

    version

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
       <List items={footerList1} mt={false} />
       <List items={footerList2} mt />
       <List items={footerList3} mt />
-      <p className="text-gray-400 text-sm mt-5">@Copyright 2022 TikTik</p>
+      <p className="text-gray-400 text-sm mt-5">@Copyright 2023 flowstream</p>
     </div>
   );
 };
